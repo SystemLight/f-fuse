81b928fbdd00db976dc02fc6e117d68f08a5edd5
SystemLight <1466335092@qq.com>
2022-07-12 14:52:06 +0800

    fix: lint code

--- src/main.ts
+++ src/main.ts
@@ -11,57 +11,51 @@ interface IFuseOptions {
   memory?: boolean
 }
 
-export default class Fuse {
-
-  private isBroken: boolean;
-  private when: IWhen;
-  private result: any;
-  public closure_arg: any;
-
+class Fuse {
+  private isBroken: boolean
+  private when: IWhen
+  private result: any
+  public closure_arg: any
 
   constructor(
     public opt: IFuseOptions = {}
   ) {
-    this.isBroken = !!this.opt.manual;
-    this.when = this.__when;
+    this.isBroken = !!this.opt.manual
+    this.when = this.__when
   }
 
   weld(): void {
-    this.isBroken = false;
+    this.isBroken = false
   }
 
   cut(): void {
-    this.isBroken = true;
+    this.isBroken = true
   }
 
   __when(...args: any[]): boolean {
-    return true;
+    return true
   }
 
   on(callback: IWhen): void {
-    this.when = callback.bind(this);
-  }
-
-  callComplete() {
+    this.when = callback.bind(this)
   }
 
   closure(...args: any[]): void {
-    this.closure_arg = args;
+    this.closure_arg = args
   }
 
   def(func: IDef): any {
-    let {manual, memory} = this.opt;
+    const {manual, memory} = this.opt
     return (...args: any[]) => {
       if (this.isBroken && memory) {
-        return this.result;
+        return this.result
       } else if (!this.isBroken && this.when(...args)) {
-        this.result = func(...args);
-        !manual && this.cut();
-        this.callComplete();
-        this.callComplete = () => {
-        };
-        return this.result;
+        this.result = func(...args)
+        !manual && this.cut()
+        return this.result
       }
-    };
+    }
   }
 }
+
+export default Fuse
